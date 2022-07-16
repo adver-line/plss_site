5b32c1bf20c14fac1aa709a555a6e8a37c4ea609
박건영 <adver@baggeon-yeong-ui-MacBookPro.local>
2022-07-16 14:59:43 +0900

    now upload-122

--- config/settings.py
+++ config/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -24,6 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-a*r5m&p2_8lb@6_c0!c1zuyr=@3ku=jsv95fjr=o(-6x=m%nkn"
 
 # SECURITY WARNING: don't run with debug turned on in production!
+
+
 DEBUG = bool(os.environ.get("DEBUG"))
 
 ALLOWED_HOSTS = []
