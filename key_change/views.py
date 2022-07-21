32b0bc3fa4f2e4150f8d83465bb57caac0bd2ff6
adver833 <adver833@gmail.com>
2022-07-21 14:05:48 +0900

    인코딩 페이지 추가 1

--- key_change/views.py
+++ key_change/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from slot import models
 import urllib.parse
-# Create your views here.
 
 
 def index(request):
@@ -12,19 +11,22 @@ def index(request):
 def key_detail(request, pk):
     key_word_slot = models.Slot.objects.get(pk=pk)
     key_words = key_word_slot.serch_key
-    key_words = "".join(key_words)
+    # key_words = "".join(key_words)
     key_words = key_words.replace(", ", ",")
     key_words = key_words.split(',')
     re_keyword_list = []
-    print(key_words)
 
     for key_word in key_words:
-        re_keyword_list.append(key_word)
         qu_key = urllib.parse.quote(key_word)
-        qu_key = f'https://m.search.naver.com/search.naver?sm=mtp_hty.top&where=m&query={qu_key}'
-        re_keyword_list.append(qu_key)
 
-    print(re_keyword_list)
+        if key_word_slot.product_choices == "단일상품":
+            qu_key = f'https://m.search.naver.com/search.naver?sm=mtp_hty.top&where=m&query={qu_key}'
+        else:
+            naver_url = key_word_slot.product_url
+            qu_key = f'{naver_url}?query={qu_key}'
+
+        key_dic = {"key":key_word, "key_url":qu_key}
+        re_keyword_list.append(key_dic)
 
     return render(request, "key_change/key_detail.html", {
         "enc_keys":re_keyword_list
